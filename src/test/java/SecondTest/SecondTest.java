5a886b167c6c0a35650fab085dc89e43b04549b8
Vasily Kabanov <kabanovasily@inbox.ru>
2019-11-16 16:01:01 +0300

    Commit 3

--- src/test/java/SecondTest/SecondTest.java
+++ src/test/java/SecondTest/SecondTest.java
@@ -4,34 +4,74 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * Created by yasup on 13.11.2019.
  */
 
+@RunWith(Parameterized.class)
 public class SecondTest {
 
     private WebDriver driver;
     private String URL;
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Ivanov", "Ivan", "01.01.1991", "Иванов", "Иван", "Иванович", "01.01.1991", "1111", "111111", "01.01.2011", "УФМС г. Пермь"},
+                {"Petrov", "Petr", "02.02.1992", "Петров", "Петр", "Петрович", "02.02.1992", "2222", "222222", "02.02.2012", "УФМС г. Москва"},
+                {"Sidorov", "Sidor", "03.03.1993", "Сидоров", "Сидор", "Сидорович", "03.03.1993", "3333", "333333", "03.03.2013", "УФМС г. Тула"}});
+    }
+
+    @Parameterized.Parameter
+    public String insured0_surname;
+    @Parameterized.Parameter(1)
+    public String insured0_name;
+    @Parameterized.Parameter(2)
+    public String insured0_birthDate;
+    @Parameterized.Parameter(3)
+    public String surname;
+    @Parameterized.Parameter(4)
+    public String name;
+    @Parameterized.Parameter(5)
+    public String middleName;
+    @Parameterized.Parameter(6)
+    public String birthDate;
+    @Parameterized.Parameter(7)
+    public String passport_series;
+    @Parameterized.Parameter(8)
+    public String passport_number;
+    @Parameterized.Parameter(9)
+    public String issueDate;
+    @Parameterized.Parameter(10)
+    public String issuePlace;
+
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "drv/chromedriver78.exe");
-        System.setProperty("webdriver.gecko.driver", "drv/geckodriver.exe");
-
         driver = new ChromeDriver();
+//        System.setProperty("webdriver.ie.driver", "drv/IEDriverServer.exe");
+//        driver = new InternetExplorerDriver();
+//        System.setProperty("webdriver.gecko.driver", "drv/geckodriver.exe");
+//        driver = new FirefoxDriver();
         URL = "https://www.sberbank.ru/ru/person";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -68,17 +108,17 @@ public class SecondTest {
         driver.findElement(By.xpath("//SPAN[@ng-click='save()'][text()='Оформить']")).click();
 
         //Застрахованные
-        clickAndSendKeys(By.xpath("//INPUT[@name='insured0_surname']"), "Petrov"); //Фамилия
-        clickAndSendKeys(By.xpath("//INPUT[@name='insured0_name']"), "Petr"); //Имя
-        clickAndSendKeys(By.xpath("//input[@name='insured0_birthDate']"), "19061991"); //Дата рождения
+        clickAndSendKeys(By.xpath("//INPUT[@name='insured0_surname']"), insured0_surname); //Фамилия
+        clickAndSendKeys(By.xpath("//INPUT[@name='insured0_name']"), insured0_name); //Имя
+        clickAndSendKeys(By.xpath("//input[@name='insured0_birthDate']"), insured0_birthDate); //Дата рождения
 
         scrollIntoView(By.xpath("//H3[@class='b-form-section-title'][text()='Страхователь']")); // Сделать скролл
 
         //Страхователь
-        clickAndSendKeys(By.xpath("//INPUT[@name='surname']"), "Иванов"); //Фамилия
-        clickAndSendKeys(By.xpath("//INPUT[@name='name']"), "Иван"); //Имя
-        clickAndSendKeys(By.xpath("//INPUT[@name='middlename']"), "Иванович");  //Отчество
-        clickAndSendKeys(By.xpath("//INPUT[@name='birthDate']"), "26041990"); //Дата рождения
+        clickAndSendKeys(By.xpath("//INPUT[@name='surname']"), surname); //Фамилия
+        clickAndSendKeys(By.xpath("//INPUT[@name='name']"), name); //Имя
+        clickAndSendKeys(By.xpath("//INPUT[@name='middlename']"), middleName);  //Отчество
+        clickAndSendKeys(By.xpath("//INPUT[@name='birthDate']"), birthDate); //Дата рождения
 
         scrollIntoView(By.xpath("//H3[@class='b-form-section-title ng-binding'][text()='Данные паспорта РФ']")); // Сделать скролл
 
@@ -86,18 +126,26 @@ public class SecondTest {
         driverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//INPUT[@name='passport_series']"))));
 
         //Паспортные данные
-        clickAndSendKeys(By.xpath("//INPUT[@name='passport_series']"),"4567"); //Серия
-        clickAndSendKeys(By.xpath("//INPUT[@name='passport_number']"),"965185"); //Номер паспорта
-        clickAndSendKeys(By.xpath("//INPUT[@name='issueDate']"),"05102017"); //Дата выдачи
-        clickAndSendKeys(By.xpath("//TEXTAREA[@name='issuePlace']"),"УФМС России"); //Кем выдан
+        clickAndSendKeys(By.xpath("//INPUT[@name='passport_series']"), passport_series); //Серия
+        clickAndSendKeys(By.xpath("//INPUT[@name='passport_number']"), passport_number); //Номер паспорта
+        clickAndSendKeys(By.xpath("//INPUT[@name='issueDate']"), issueDate); //Дата выдачи
+        clickAndSendKeys(By.xpath("//TEXTAREA[@name='issuePlace']"), issuePlace); //Кем выдан
 
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='insured0_surname']")).getAttribute("value"), equalTo(insured0_surname));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='insured0_name']")).getAttribute("value"), equalTo(insured0_name));
+        assertThat(driver.findElement(By.xpath("//input[@name='insured0_birthDate']")).getAttribute("value"), equalTo(insured0_birthDate));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='surname']")).getAttribute("value"), equalTo(surname));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='name']")).getAttribute("value"), equalTo(name));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='middlename']")).getAttribute("value"), equalTo(middleName));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='birthDate']")).getAttribute("value"), equalTo(birthDate));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='passport_series']")).getAttribute("value"), equalTo(passport_series));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='passport_number']")).getAttribute("value"), equalTo(passport_number));
+        assertThat(driver.findElement(By.xpath("//INPUT[@name='issueDate']")).getAttribute("value"), equalTo(issueDate));
+        assertThat(driver.findElement(By.xpath("//TEXTAREA[@name='issuePlace']")).getAttribute("value"), equalTo(issuePlace));
         driver.findElement(By.xpath("//SPAN[@ng-click='save()'][text()='Продолжить']")).click();
-        Thread.sleep(3000);
-
         Assert.assertEquals("Заполнены не все обязательные поля",
                 driver.findElement(By.xpath("//DIV[@ng-show='tryNext && myForm.$invalid'][text()='Заполнены не все обязательные поля']"))
                         .getText());
-
         System.out.println("тест пройден!");
     }
 
